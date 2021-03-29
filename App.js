678576a1d999b0a183f699791b2120948bffcf13
Princess Kena <kenaprincess3@gmail.com>
2021-03-29 03:03:30 -0700

    created a profile

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text,  View, StyleSheet, Image} from 'react-native';
+ import profile from './assets/profile.jpg'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function App(){
+return <View style={styles.container}>
+        <Image source={profile} style={styles.image}/>
+  
+  <View style={styles.profileDetails}>
+    <Text style={styles.label}>Name</Text>
+    <Text style={styles.info}>Shakana Page</Text>
+  </View>
+
+  <View style={styles.profileDetails}>
+    <Text style={styles.label}>Email</Text>
+    <Text style={styles.info}>Shakana@gmail.com</Text>
+  </View>
+
+  <View style={styles.profileDetails}>
+    <Text style={styles.label}>Gender</Text>
+    <Text style={styles.info}>Female</Text>
+  </View>
+
+</View>
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+container:{
+  flex :1,
+  marginHorizontal:10
+},
+image:{
+  width:180,
+  height:180,
+  marginBottom:30,
+  alignSelf:'center',
+  borderRadius:90,
+marginTop:150,
+  
+},
+profileDetails:{
+  flexDirection:"row",
+  marginVertical: 3,
+},
+label:{
+  borderColor:"black",
+  borderWidth:1,
+  flex :2,
+  fontSize:20,
+  paddingHorizontal:5,
+},
+info:{
+flex:8,
+borderColor:"black",
+borderWidth:1,
+  fontSize:20,
+  paddingHorizontal:5,
+}
+   
 });
+
+
+export default App;
